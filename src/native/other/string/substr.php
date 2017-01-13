cdf2fc1a3457d5c9f1ab0ca533614c482d15ae10
Marcus Chiu <marcus.chiu@kibocommerce.com>
2017-01-12 21:53:53 -0600

    added more substr examples

--- src/native/other/string/substr.php
+++ src/native/other/string/substr.php
@@ -1,12 +1,24 @@
 <?php
 // substr() returns part of a string
 
-$postal = "12345-1234";
+$postal = "12345-6789";
+echo "postal string: " . $postal;
+echo "<div/>";
 
 $start_index = 0;
 $end_index = 5;
 $postal_comp = substr($postal, $start_index, $end_index);
+echo "substr($postal, $start_index, $end_index): " . $postal_comp;
+echo "<div/>";
 
-echo $postal_comp;
+$index = 4;
+$substr = substr($postal, $index);
+echo "substr($postal, $index): " . $substr;
+echo "<div/>";
+
+$index = -4;
+$substr = substr($postal, $index);
+echo "substr($postal, $index): " . $substr;
+echo "<div/>";
 
 ?>
